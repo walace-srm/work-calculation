b599c7b4ab46404bcd63b932abd2019654244263
Walace Maia <walace_srm@hotmail.com>
2022-03-27 19:30:52 -0300

    feat(inss): tabela 2022

--- src/app/pages/calc-inss/calc-inss.page.ts
+++ src/app/pages/calc-inss/calc-inss.page.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {ToastController} from '@ionic/angular';
-import {PDFGenerator} from '@ionic-native/pdf-generator/ngx';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ToastController } from "@ionic/angular";
+import { PDFGenerator } from "@ionic-native/pdf-generator/ngx";
 
 @Component({
-  selector: 'app-calc-inss',
-  templateUrl: './calc-inss.page.html',
-  styleUrls: ['./calc-inss.page.scss'],
+  selector: "app-calc-inss",
+  templateUrl: "./calc-inss.page.html",
+  styleUrls: ["./calc-inss.page.scss"],
 })
-export class CalcInssPage  {
-
+export class CalcInssPage {
   public inssForm: FormGroup;
   public grossSalary: number;
   public grossSalaryFormatted: string;
@@ -19,65 +18,72 @@ export class CalcInssPage  {
   public aliquota: any;
 
   constructor(
-      private formBuilder: FormBuilder,
-      public toastController: ToastController,
-      private pdfGenerator: PDFGenerator
+    private formBuilder: FormBuilder,
+    public toastController: ToastController,
+    private pdfGenerator: PDFGenerator
   ) {
     this.inssForm = this.formBuilder.group({
-      grossSalary: ['', Validators.required],
-      inssValue: ['']
+      grossSalary: ["", Validators.required],
+      inssValue: [""],
     });
   }
 
   calcInss() {
-    if (this.grossSalary < 1100) {
+    if (this.grossSalary < 1212) {
       this.lowerSalaryToast();
       this.calculatedInss = undefined;
     }
-    if (this.grossSalary === 1100) {
-      this.inssValue = 82.50;
-      this.calculatedInss = 82.50;
+    if (this.grossSalary === 1212) {
+      this.inssValue = 90.9;
+      this.calculatedInss = 90.9;
       this.aliquota = 7.5;
-    } else if ((this.grossSalary > 1100) && (this.grossSalary < 2203.49)) {
-      this.inssValue = 99.31;
-      this.calculatedInss = (this.grossSalary * 9 / 100) - 16.50;
+    } else if (this.grossSalary > 1212 && this.grossSalary < 2427.35) {
+      this.inssValue = 109.38;
+      this.calculatedInss = (this.grossSalary * 9) / 100 - 18.18;
       this.aliquota = 9;
-    } else if ((this.grossSalary > 2203.48) && (this.grossSalary < 3305.23)) {
-      this.inssValue = 132.21;
-      this.calculatedInss = (this.grossSalary * 12 / 100) - 82.60;
+    } else if (this.grossSalary > 2427.36 && this.grossSalary < 3641.03) {
+      this.inssValue = 145.64;
+      this.calculatedInss = (this.grossSalary * 12) / 100 - 91;
       this.aliquota = 12;
-    } else if ((this.grossSalary > 3305.22) && (this.grossSalary < 6433.58)) {
-      this.inssValue = 437.96;
-      this.calculatedInss = (this.grossSalary * 14 / 100) - 148.70;
+    } else if (this.grossSalary > 3641.04 && this.grossSalary < 7087.22) {
+      this.inssValue = 482.47;
+      this.calculatedInss = (this.grossSalary * 14) / 100 - 163.82;
       this.aliquota = 14;
-    } else if ((this.grossSalary > 6433.57)) {
-      this.inssValue = 437.96;
-      this.calculatedInss = (6433.57 * 14 / 100) - 148.70;
-      this.aliquota = 'Teto';
+    } else if (this.grossSalary > 7087.22) {
+      this.inssValue = 482.47;
+      this.calculatedInss = (7087.22 * 14) / 100 - 163.82;
+      this.aliquota = "Teto";
     }
     this.inssForm.patchValue({
-      inssValue: this.calculatedInss?.toLocaleString('pt-BR', { maximumFractionDigits: 2, minimumFractionDigits: 2 })
+      inssValue: this.calculatedInss?.toLocaleString("pt-BR", {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2,
+      }),
     });
-    this.calculatedInssPdf = this.calculatedInss
-        ?.toLocaleString('pt-BR', { maximumFractionDigits: 2, minimumFractionDigits: 2 });
-    this.grossSalaryFormatted = this.grossSalary
-        .toLocaleString('pt-BR', { maximumFractionDigits: 2, minimumFractionDigits: 2 });
+    this.calculatedInssPdf = this.calculatedInss?.toLocaleString("pt-BR", {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2,
+    });
+    this.grossSalaryFormatted = this.grossSalary.toLocaleString("pt-BR", {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2,
+    });
   }
 
   async lowerSalaryToast() {
     const toast = await this.toastController.create({
-      message: 'Valor inferior ao salário mínimo (R$1.100)',
+      message: "Valor inferior ao salário mínimo (R$1.100)",
       duration: 3000,
-      color: 'medium'
+      color: "medium",
     });
     toast.present();
   }
 
   async reportSalaryToast() {
     const toast = await this.toastController.create({
-      message: 'Insira o valor do salário!',
+      message: "Insira o valor do salário!",
       duration: 3000,
-      color: 'medium'
+      color: "medium",
     });
     toast.present();
   }
@@ -92,21 +98,21 @@ export class CalcInssPage  {
     this.grossSalary = e;
     if (!e) {
       this.inssForm.patchValue({
-        inssValue: this.inssForm.value.inssValue = undefined
+        inssValue: (this.inssForm.value.inssValue = undefined),
       });
       this.calculatedInss = undefined;
     }
   }
 
   generatorPdf() {
-    const hidden = document.getElementById('remove');
-    hidden.removeAttribute('hidden');
-    hidden.setAttribute('hidden', '');
-    const a: any = document.getElementById('print');
-    this.pdfGenerator.fromData(a.innerHTML, { type: 'share' });
+    const hidden = document.getElementById("remove");
+    hidden.removeAttribute("hidden");
+    hidden.setAttribute("hidden", "");
+    const a: any = document.getElementById("print");
+    this.pdfGenerator.fromData(a.innerHTML, { type: "share" });
   }
 
-  onSubmit({ value, valid}: {value: any; valid: boolean }) {
+  onSubmit({ value, valid }: { value: any; valid: boolean }) {
     if (!valid) {
       return this.reportSalaryToast();
     }
---
+++ src/app/utils/util.ts
@@ -0,0 +1,2 @@
+//TODO: Implementar para evitar repetição de codigo
+export function calcInss() {}
